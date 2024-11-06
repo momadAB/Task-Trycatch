116d6196c6fd43e809499d70be23dde4ac97e5be
momadAB <mabaqer99@gmail.com>
2024-11-06 14:46:24 +0300

    Finished bonus tasks

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,17 @@ public class Main {
         // Predefined login credentials
         String validUsername = "Coded";
         String validPassword = "Coded123";
+        boolean userLoggedIn = false;
 
-        for (int loginAttempts = 0; loginAttempts < 5; loginAttempts++) {
+        for (int loginAttempts = 0; !userLoggedIn; loginAttempts++) {
             try {
                 System.out.print("Enter username: ");
                 String username = scanner.nextLine();
                 System.out.print("Enter password: ");
                 String password = scanner.nextLine();
                 if (username.equals(validUsername) && password.equals(validPassword)) {
-                    System.out.printf("Welcome %s!%n", validUsername);
-                    break;
+                    System.out.printf("Welcome %s!%n", username);
+                    userLoggedIn = true;
                 }
                 // If failed to log in while on last attempt.
                 else if (loginAttempts == 4) {
@@ -27,9 +28,10 @@ public class Main {
                 }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
+                if (e.getMessage().equals("Maximum attempts exceeded."))
+                    break;
             }
         }
-
         scanner.close();
     }
 }
