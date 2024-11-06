a09503b41d24d49020cff16f683cca3206b22f2d
momadAB <mabaqer99@gmail.com>
2024-11-06 14:08:14 +0300

    Finished bonus

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Predefined login credentials
+        String validUsername = "Coded";
+        String validPassword = "Coded123";
+
+        // I know it says to use a for loop but this feels way easier
+        for (int loginAttempts = 0; loginAttempts < 5; loginAttempts++) {
+            try {
+                System.out.print("Enter username: ");
+                String username = scanner.nextLine();
+                System.out.print("Enter password: ");
+                String password = scanner.nextLine();
+                if (username.equals(validUsername) && password.equals(validPassword)) {
+                    System.out.printf("Welcome %s!%n", validUsername);
+                    break;
+                }
+                // If failed to log in while on last attempt.
+                else if (loginAttempts == 4) {
+                    throw new Exception("Maximum attempts exceeded.");
+                } else {
+                    // If failed to log in
+                    throw new Exception("Invalid Credentials");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        scanner.close();
+    }
+}
